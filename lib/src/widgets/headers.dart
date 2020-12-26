2d6460ecd6ec9054a904d70ffc8da63020b39edc
Gonzalo Campos <erick.campos@wizeline.com>
2020-12-26 17:22:23 -0600

    Create header with border radius.

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -9,3 +9,19 @@ class SquaredHeader extends StatelessWidget {
     );
   }
 }
+
+class CircularBorderHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(50),
+          bottomRight: Radius.circular(50),
+        ),
+        color: Color(0xff615AAB),
+      ),
+      height: 300.0,
+    );
+  }
+}

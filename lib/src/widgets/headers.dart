3f83ff341100849c68aef6aefca479131e09d327
Gonzalo Campos <erick.campos@wizeline.com>
2020-12-26 17:48:24 -0600

    Create a diagonal header using CustomPaint.

--- lib/src/widgets/headers.dart
+++ lib/src/widgets/headers.dart
@@ -25,3 +25,45 @@ class CircularBorderHeader extends StatelessWidget {
     );
   }
 }
+
+class DiagonalHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: double.infinity,
+      width: double.infinity,
+      child: CustomPaint(
+        painter: _DiagonalHeaderPainter(),
+      ),
+    );
+  }
+}
+
+class _DiagonalHeaderPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint();
+
+    // some properties
+    paint.color = Color(0xff615AAB);
+    // PaintingStyle.stroke --> draw only the borders
+    // PaintingStyle.fill --> draw the whole figure
+    paint.style = PaintingStyle.fill;
+    paint.strokeWidth = 2;
+
+    // variable that says how the Paint is going to be moved
+    final path = Path();
+
+    // draw with Path and Paint (moveTo and lineTo)
+    path.moveTo(0, size.height * 0.35);
+    path.lineTo(size.width, size.height * 0.30);
+    path.lineTo(size.width, 0);
+    path.lineTo(0, 0);
+
+    // draw in canvas
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
+}

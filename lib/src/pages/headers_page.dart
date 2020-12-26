4ad8eb2ac657cc204988b0b0030b412bd960ed83
Gonzalo Campos <erick.campos@wizeline.com>
2020-12-26 17:18:37 -0600

    Create simple squared header.

---
+++ lib/src/pages/headers_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:intermediate_flutter/src/widgets/headers.dart';
+
+class HeadersPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SquaredHeader(),
+    );
+  }
+}

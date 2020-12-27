6f638ef4ecf0fd166970eb26168aba300edba116
Gonzalo Campos <erick.campos@wizeline.com>
2020-12-26 21:14:25 -0600

    Create navbar to see all the custom painters.

--- lib/src/pages/headers_page.dart
+++ lib/src/pages/headers_page.dart
@@ -1,11 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:intermediate_flutter/src/widgets/headers.dart';
 
-class HeadersPage extends StatelessWidget {
+class HeadersPage extends StatefulWidget {
+  @override
+  _HeadersPageState createState() => _HeadersPageState();
+}
+
+class _HeadersPageState extends State<HeadersPage> {
+  int _currentHeaderIndex = 0;
+  List<Widget> _headersList = [
+    SquaredHeader(),
+    CircularBorderHeader(),
+    DiagonalHeader(),
+    DiamondHeader(),
+    PeakHeader(),
+    CurveHeader(),
+    WaveHeader(),
+    WaveGradientHeader(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: WaveGradientHeader(),
+      bottomNavigationBar: BottomAppBar(
+        child: BottomNavigationBar(
+          elevation: 5.0,
+          unselectedItemColor: Colors.grey,
+          currentIndex: _currentHeaderIndex,
+          onTap: _updateIndex,
+          selectedItemColor: Color(0xff6D05FA),
+          items: [
+            BottomNavigationBarItem(
+                label: 'Squared', icon: Icon(Icons.gradient)),
+            BottomNavigationBarItem(
+                label: 'Circular', icon: Icon(Icons.gradient)),
+            BottomNavigationBarItem(
+                label: 'Diagonal', icon: Icon(Icons.gradient)),
+            BottomNavigationBarItem(
+                label: 'Diamond', icon: Icon(Icons.gradient)),
+            BottomNavigationBarItem(
+                label: 'Peak',
+                icon: Icon(
+                  Icons.gradient,
+                )),
+            BottomNavigationBarItem(label: 'Curve', icon: Icon(Icons.gradient)),
+            BottomNavigationBarItem(label: 'Wave', icon: Icon(Icons.gradient)),
+            BottomNavigationBarItem(
+                label: 'Gradient', icon: Icon(Icons.gradient)),
+          ],
+        ),
+      ),
+      body: IndexedStack(
+        index: _currentHeaderIndex,
+        children: _headersList,
+      ),
     );
+  }
+
+  void _updateIndex(int newIndex) {
+    setState(() {
+      _currentHeaderIndex = newIndex;
+    });
   }
 }

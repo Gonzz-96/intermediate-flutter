32788638596f83b2859a333302c5e683544df3c4
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-02 23:36:19 -0600

    Add custom transition to splash screen.

--- lib/src/pages/splash_page.dart
+++ lib/src/pages/splash_page.dart
@@ -40,8 +40,27 @@ class SplashPage extends StatelessWidget {
 
   void _navigateTo(Widget screen, BuildContext context) => Navigator.push(
         context,
-        MaterialPageRoute(builder: (context) => screen),
+        _createRoute(screen),
       );
+
+  PageRoute _createRoute(Widget screen) {
+    return PageRouteBuilder(
+      pageBuilder: (context, animation, secondaryAnimation) => screen,
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = Offset(0.0, 1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
 }
 
 class _FlatButton extends StatelessWidget {

b37df12ac17f38b31570c772a6ea555b14ca6bbe
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-02 20:10:32 -0600

    Use CustomPainter to draw a circle and an arc.

--- lib/src/pages/circular_progress_page.dart
+++ lib/src/pages/circular_progress_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math' as Math;
+
 import 'package:flutter/material.dart';
 
 class CircularProgressPage extends StatefulWidget {
@@ -11,11 +13,56 @@ class _CircularProgressPageState extends State<CircularProgressPage> {
     return Scaffold(
       body: Center(
         child: Container(
+          padding: EdgeInsets.all(5.0),
           width: 300,
           height: 300,
-          color: Colors.red,
+          child: CustomPaint(
+            painter: _RadialProgressPainter(arcPercentage: 50),
+          ),
         ),
       ),
     );
   }
+}
+
+class _RadialProgressPainter extends CustomPainter {
+  _RadialProgressPainter({this.arcPercentage = 0});
+
+  double arcPercentage;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // complete circle
+    final circlePaint = Paint()
+      ..strokeWidth = 4.0
+      ..color = Colors.grey
+      ..style = PaintingStyle.stroke;
+
+    final centerOffset = Offset(size.width * .5, size.height * 0.5);
+    final radius = Math.min(size.width * 0.5, size.height * 0.5);
+
+    canvas.drawCircle(centerOffset, radius, circlePaint);
+
+    // arc
+    final arcPaint = Paint()
+      ..strokeWidth = 10.0
+      ..color = Colors.pink
+      ..style = PaintingStyle.stroke;
+
+    // filled part
+    // final centerOffset = Offset(size.width * .5, size.height * 0.5);
+    // final radio = min(size.width * 0.5, size.height * 0.5);
+    var arcAngle = 2 * Math.pi * (arcPercentage / 100);
+
+    canvas.drawArc(
+      Rect.fromCircle(center: centerOffset, radius: radius),
+      -Math.pi / 2,
+      arcAngle,
+      false,
+      arcPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }

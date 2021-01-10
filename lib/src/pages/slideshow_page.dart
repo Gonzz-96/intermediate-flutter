2165065a7fe81ebcf8fc31aadc814993edcf83a4
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-10 16:58:36 -0600

    Add a PageView to see multiple images.

--- lib/src/pages/slideshow_page.dart
+++ lib/src/pages/slideshow_page.dart
@@ -5,8 +5,39 @@ class SlideShowPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('Hola, mundo'),
+        child: _Slides(),
       ),
+    );
+  }
+}
+
+class _Slides extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: PageView(
+        children: [
+          _Slide('assets/slides/1.png'),
+          _Slide('assets/slides/2.png'),
+          _Slide('assets/slides/3.png')
+        ],
+      ),
+    );
+  }
+}
+
+class _Slide extends StatelessWidget {
+  _Slide(this.image);
+
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      padding: EdgeInsets.all(15),
+      child: Image.asset(image),
     );
   }
 }

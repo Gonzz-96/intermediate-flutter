d296378749c8de33341a5dc4541a4b2b3910b2e1
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-10 17:54:14 -0600

    Move slideshow_page to labs package.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:intermediate_flutter/src/pages/slideshow_page.dart';
+import 'package:intermediate_flutter/src/labs/slideshow_page.dart';
 import 'package:intermediate_flutter/src/pages/splash_page.dart';
 
 void main() {

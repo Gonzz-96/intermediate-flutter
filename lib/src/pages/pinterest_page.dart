acf516ca7cb250dadf965dec5634c23b93120cb1
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-11 18:08:51 -0600

    Create pinterest base grid.

---
+++ lib/src/pages/pinterest_page.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class PinterestPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _PinterestGrid(),
+    );
+  }
+}
+
+class _PinterestGrid extends StatelessWidget {
+  final items = List.generate(200, (index) => index);
+
+  @override
+  Widget build(BuildContext context) {
+    return StaggeredGridView.countBuilder(
+      crossAxisCount: 4,
+      itemCount: items.length,
+      itemBuilder: (context, index) => _PinterestItem(index: index),
+      staggeredTileBuilder: (int index) =>
+          new StaggeredTile.count(2, index.isEven ? 2 : 3),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+    );
+  }
+}
+
+class _PinterestItem extends StatelessWidget {
+  _PinterestItem({this.index});
+
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.blue,
+      ),
+      child: new Center(
+        child: new CircleAvatar(
+          backgroundColor: Colors.white,
+          child: new Text('$index'),
+        ),
+      ),
+    );
+  }
+}

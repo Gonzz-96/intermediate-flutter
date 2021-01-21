be28e50d3dbeb41133b23dc2ff0c02f4244b22c2
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-21 16:16:44 -0600

    Adding content to the icon haeder.

--- lib/src/widgets/icon_header.dart
+++ lib/src/widgets/icon_header.dart
@@ -1,6 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class IconHeader extends StatelessWidget {
+  final _translucentWhite = Colors.white.withOpacity(0.7);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      _IconHeaderBackground(),
+      Positioned(
+        child: FaIcon(
+          FontAwesomeIcons.plus,
+          size: 250,
+          color: Colors.white.withOpacity(0.2),
+        ),
+        top: -50,
+        left: -70,
+      ),
+      Column(
+        children: [
+          SizedBox(height: 80, width: double.infinity),
+          Text(
+            'Has solicitado',
+            style: TextStyle(fontSize: 20, color: _translucentWhite),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Asistencia Médica',
+            style: TextStyle(
+              fontSize: 25,
+              color: _translucentWhite,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 20),
+          FaIcon(FontAwesomeIcons.plus, size: 80, color: Colors.white)
+        ],
+      )
+    ]);
+  }
+}
+
+class _IconHeaderBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(

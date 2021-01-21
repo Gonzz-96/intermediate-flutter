4aa2fb0d4658e0512075e8fa8665ed5c3c4c675f
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-21 17:35:38 -0600

    Add parameters to SectionButton.

--- lib/src/pages/emergency_page.dart
+++ lib/src/pages/emergency_page.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intermediate_flutter/src/widgets/icon_header.dart';
+import 'package:intermediate_flutter/src/widgets/section_button.dart';
 
 class EmergencyPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SectionButton(
+          text: 'Motor Accident',
+          iconData: FontAwesomeIcons.carCrash,
+          onPress: () {},
+        ),
+      ),
+    );
+  }
+}
+
+class PageHeader extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/src/widgets/section_button.dart
+++ lib/src/widgets/section_button.dart
@@ -2,20 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SectionButton extends StatelessWidget {
+  final String text;
+  final Function onPress;
+  final IconData iconData;
+  final Color initialColor;
+  final Color endColor;
+
+  const SectionButton({
+    @required this.text,
+    @required this.onPress,
+    this.iconData = FontAwesomeIcons.circle,
+    this.initialColor = const Color(0xFF6989F5),
+    this.endColor = const Color(0xFF906EF5),
+  });
+
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: AlignmentDirectional.center,
       children: [
-        _SectionButtonBackground(),
+        _SectionButtonBackground(initialColor, endColor, iconData),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SizedBox(height: 140, width: 40),
-            FaIcon(FontAwesomeIcons.carCrash, color: Colors.white, size: 40),
-            SizedBox(height: 140, width: 20),
+            SizedBox(width: 40),
+            FaIcon(iconData, color: Colors.white, size: 40),
+            SizedBox(width: 20),
             Expanded(
               child: Text(
-                'Motor Accident',
+                text,
                 style: TextStyle(color: Colors.white, fontSize: 18),
               ),
             ),
@@ -29,6 +44,16 @@ class SectionButton extends StatelessWidget {
 }
 
 class _SectionButtonBackground extends StatelessWidget {
+  final Color initialColor;
+  final Color endColor;
+  final IconData iconData;
+
+  const _SectionButtonBackground(
+    this.initialColor,
+    this.endColor,
+    this.iconData,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -40,23 +65,20 @@ class _SectionButtonBackground extends StatelessWidget {
               right: -20,
               top: -20,
               child: FaIcon(
-                FontAwesomeIcons.carCrash,
-                color: Colors.white.withOpacity(0.2),
+                iconData,
                 size: 150,
+                color: Colors.white.withOpacity(0.2),
               ),
             ),
           ],
         ),
       ),
-      height: 100,
+      height: MediaQuery.of(context).size.height / 9,
       width: double.infinity,
       margin: EdgeInsets.all(20),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(colors: [
-          Color(0xFF6989F5),
-          Color(0xFF906EF5),
-        ]),
+        gradient: LinearGradient(colors: [initialColor, endColor]),
         boxShadow: [
           BoxShadow(
               color: Colors.black.withOpacity(0.2),

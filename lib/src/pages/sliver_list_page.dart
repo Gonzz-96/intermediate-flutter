3e1b94a679856626884ba2f60298072b5830b709
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-21 23:19:11 -0600

    Create title of the page.

--- lib/src/pages/sliver_list_page.dart
+++ lib/src/pages/sliver_list_page.dart
@@ -4,9 +4,50 @@ class SliverListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _StuffList(),
-      ),
+      body: _Title(),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 30),
+        Container(
+          margin: EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+          child: Text(
+            'New',
+            style: TextStyle(
+              color: Color(0xFF532128),
+              fontSize: 50,
+            ),
+          ),
+        ),
+        Stack(
+          children: [
+            Positioned(
+              bottom: 0,
+              child: Container(
+                width: 120,
+                height: 8,
+                decoration: BoxDecoration(color: Color(0xFFF7CDD5)),
+              ),
+            ),
+            Container(
+              child: Text(
+                'List',
+                style: TextStyle(
+                  color: Color(0xFFD93A30),
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        )
+      ],
     );
   }
 }

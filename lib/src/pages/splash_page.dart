b055d8540cb1e6b6e42c20567e4a92bccac363f3
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-25 13:25:33 -0600

    Improve splash screen.

--- lib/src/pages/splash_page.dart
+++ lib/src/pages/splash_page.dart
@@ -24,24 +24,26 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Intermediate Flutter Course'),
-        backgroundColor: Colors.black,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(60.0),
-        child: ListView.builder(
-          itemCount: _screens.length,
-          itemBuilder: (context, index) {
-            final pair = _screens.entries.toList()[index];
-
-            return _FlatButton(
-              text: pair.key,
-              onTap: () {
-                _navigateTo(pair.value, context);
-              },
-            );
-          },
+        title: Text(
+          'Intermediate Flutter Course',
+          style: TextStyle(color: Colors.black),
         ),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView.builder(
+        physics: BouncingScrollPhysics(),
+        itemCount: _screens.length,
+        itemBuilder: (context, index) {
+          final pair = _screens.entries.toList()[index];
+
+          return _ScreensItem(
+            text: pair.key,
+            onTap: () {
+              _navigateTo(pair.value, context);
+            },
+            shouldHaveDivider: index != _screens.length - 1,
+          );
+        },
       ),
     );
   }
@@ -52,35 +54,42 @@ class SplashPage extends StatelessWidget {
       );
 }
 
-class _FlatButton extends StatelessWidget {
-  const _FlatButton({
+class _ScreensItem extends StatelessWidget {
+  const _ScreensItem({
     this.text,
     this.onTap,
+    this.shouldHaveDivider,
   });
 
   final String text;
   final Function onTap;
+  final bool shouldHaveDivider;
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 30.0),
-      child: FlatButton(
-        height: 50,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0),
-        ),
-        onPressed: onTap,
-        color: Colors.black,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30.0),
-          child: Text(
-            text,
-            style: TextStyle(
-              color: Colors.white,
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Text(
+              text,
+              style: TextStyle(
+                fontSize: 16,
+                color: Colors.black,
+                fontWeight: FontWeight.w500,
+              ),
             ),
           ),
-        ),
+          if (shouldHaveDivider)
+            Container(
+              width: double.infinity,
+              height: 1,
+              color: Colors.black.withOpacity(0.3),
+            )
+        ],
       ),
     );
   }

378d66323762dce5edafeb7906fc8023d846c9e9
Gonzalo Campos <erick.campos@wizeline.com>
2021-01-27 21:06:03 -0600

    Add drawer to splash screen.

--- lib/src/pages/splash_page.dart
+++ lib/src/pages/splash_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intermediate_flutter/src/challenges/animated_square_screen.dart';
 import 'package:intermediate_flutter/src/pages/animate_do_page.dart';
 import 'package:intermediate_flutter/src/pages/animations_page.dart';
@@ -10,49 +11,67 @@ import 'package:intermediate_flutter/src/pages/slideshow_page.dart';
 import 'package:intermediate_flutter/src/pages/sliver_list_page.dart';
 
 class SplashPage extends StatelessWidget {
-  final _screens = {
-    'Custom Painter': HeadersPage(),
-    'Custom Animations': AnimationsPage(),
-    'Animations Challenge': AnimatesSquaredScreen(),
-    'Circular Graphics': CircularGraphicsPage(),
-    'Slideshow Page': SlideshowPage(),
-    'Pinterest Page': PinterestPage(),
-    'Emergency Layout': EmergencyPage(),
-    'Sliver List / Appbar': SliverListPage(),
-    'Animate Do': AnimateDoPage(),
-  };
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: _PrincipalMenu(),
       appBar: AppBar(
-        title: Text(
-          'Intermediate Flutter Course',
-          style: TextStyle(color: Colors.black),
-        ),
-        backgroundColor: Colors.white,
+        title: Text('Intermediate Flutter Course'),
+        backgroundColor: Colors.blue,
       ),
-      body: ListView.builder(
-        physics: BouncingScrollPhysics(),
-        itemCount: _screens.length,
-        itemBuilder: (context, index) {
-          final pair = _screens.entries.toList()[index];
-          return _ScreensItem(
-            text: pair.key,
-            onTap: () {
-              _navigateTo(pair.value, context);
-            },
-            shouldHaveDivider: index != _screens.length - 1,
-          );
-        },
+      body: _ScreensList(),
+    );
+  }
+}
+
+class _PrincipalMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        child: Column(
+          children: [
+            SafeArea(
+              child: Container(
+                width: double.infinity,
+                height: 100,
+                child: CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  child: Text(
+                    'GC',
+                    style: TextStyle(fontSize: 50),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(child: _ScreensList()),
+            ListTile(
+              leading: Icon(Icons.lightbulb, color: Colors.blueAccent),
+              trailing: Switch.adaptive(
+                value: true,
+                onChanged: null,
+                activeColor: Colors.blueAccent,
+              ),
+              title: Text('Dark Mode'),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              child: ListTile(
+                leading:
+                    Icon(Icons.add_to_home_screen, color: Colors.blueAccent),
+                trailing: Switch.adaptive(
+                  value: true,
+                  onChanged: null,
+                  activeColor: Colors.blueAccent,
+                ),
+                title: Text('Custom Theme'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-
-  void _navigateTo(Widget screen, BuildContext context) => Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => screen),
-      );
 }
 
 class _ScreensItem extends StatelessWidget {
@@ -73,6 +92,10 @@ class _ScreensItem extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
+            leading: FaIcon(
+              FontAwesomeIcons.slideshare,
+              color: Colors.blueAccent,
+            ),
             title: Padding(
               padding: const EdgeInsets.all(25.0),
               child: Text(
@@ -86,7 +109,10 @@ class _ScreensItem extends StatelessWidget {
             ),
             trailing: Padding(
               padding: const EdgeInsets.all(10.0),
-              child: Icon(Icons.chevron_right),
+              child: Icon(
+                Icons.chevron_right,
+                color: Colors.blueAccent,
+              ),
             ),
           ),
           if (shouldHaveDivider)
@@ -99,4 +125,41 @@ class _ScreensItem extends StatelessWidget {
       ),
     );
   }
+}
+
+class _ScreensList extends StatelessWidget {
+  final _screens = {
+    'Custom Painter': HeadersPage(),
+    'Custom Animations': AnimationsPage(),
+    'Animations Challenge': AnimatesSquaredScreen(),
+    'Circular Graphics': CircularGraphicsPage(),
+    'Slideshow Page': SlideshowPage(),
+    'Pinterest Page': PinterestPage(),
+    'Emergency Layout': EmergencyPage(),
+    'Sliver List / Appbar': SliverListPage(),
+    'Animate Do': AnimateDoPage(),
+  };
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: BouncingScrollPhysics(),
+      itemCount: _screens.length,
+      itemBuilder: (context, index) {
+        final pair = _screens.entries.toList()[index];
+        return _ScreensItem(
+          text: pair.key,
+          onTap: () {
+            _navigateTo(pair.value, context);
+          },
+          shouldHaveDivider: index != _screens.length - 1,
+        );
+      },
+    );
+  }
+
+  void _navigateTo(Widget screen, BuildContext context) => Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => screen),
+      );
 }
